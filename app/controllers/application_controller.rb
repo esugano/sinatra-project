6afb4fde437936800220c032690cea3a92810761
Emily S <emily.sugano@gmail.com>
2019-02-26 14:21:03 +0000

    Set up environment

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    # enable :sessions
+    # set :session_secret, "secret"
+  end
+
+  get '/' do
+    "Test test test test test"
+  end
+end
